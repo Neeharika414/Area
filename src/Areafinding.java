2af826694f9af1fb982638216ddd6661a9070726
Neeharika414 <141895649+Neeharika414@users.noreply.github.com>
2023-10-19 12:22:23 +0530

    Add files via upload

---
+++ src/Areafinding.java
@@ -0,0 +1,13 @@
+
+public class Areafinding {
+
+	public static void main(String[] args) {
+		 Rectangle obj1 = new Rectangle(20, 20);
+        obj1.area();
+        Circle obj2 = new Circle(20);
+        obj2.area();
+        Triangle obj3 = new Triangle(20, 20);
+        obj3.area();
+	}
+
+}
